fc65a75c1ce143fe5889290488bfd401063ce540
Ricardo Salim <ricardosalim7@hotmail.com>
2020-08-08 14:34:41 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tcg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tcg-pokemon';
+}
